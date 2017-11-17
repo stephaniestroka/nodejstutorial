44e6aa4ca64b3cb5fed1958dd43b55b24784b0ad
Stephanie Stroka <stephanie.stroka@digitalid.net>
2017-11-17 11:58:14 +0100

    Returning index.html and file resources that exist under this directory

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 'use strict';
 
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function returnFile(res, filename) {
+    fs.readFile(filename, function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+}
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    console.log("Looking for " + filename);
+    
+    if (!fs.existsSync(filename) || !fs.lstatSync(filename).isFile()) {
+        returnFile(res, 'index.html');
+    } else {
+        returnFile(res, filename);
+    }
 }).listen(8080);
