cb73e1846999616314c5bd96de9bd2d52401b242
Stephanie Stroka <stephanie.stroka@digitalid.net>
2017-11-18 09:49:26 +0100

    Fetching data from the sqlite database

--- api/messages.js
+++ api/messages.js
@@ -7,19 +7,19 @@ const db = new sqlite3.Database('./tutorial.db');
 db.get("SELECT COUNT(*) AS tableCount FROM sqlite_master WHERE type='table' AND name='messages';", [], (err, row) => {
     if (err) throw err;
     if (row.tableCount == 0) {
-        db.run('CREATE TABLE messages (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, message TEXT)');
+        db.run('CREATE TABLE messages (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, message TEXT, date INT)');
     }
 });
 
 exports.addMessage = function(message) {
-    var sql = "INSERT INTO messages (name, message) VALUES (?, ?)";
+    var sql = "INSERT INTO messages (name, message, date) VALUES (?, ?, CURRENT_TIMESTAMP)";
     db.run(sql, [message.name, message.message], function(err){
         if (err) throw err;
     });
 }
 
 exports.getMessages = function(onresult) {
-    db.all("SELECT * FROM messages", [], (err, rows) => {
+    db.all("SELECT name, message FROM messages ORDER BY datetime(\"date\") DESC LIMIT 15", [], (err, rows) => {
         if (err) throw err;
         onresult(rows);
     });
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
-//const URL = "https://psbackend.herokuapp.com/api/messages/";
-const URL = "localhost:8080/api/messages/";
+const URL = "https://psbackend.herokuapp.com/api/messages/";
+//const URL = "http://localhost:8080/api/messages/";
+
+function fetchData() {
+    $.get(URL, function(data) {
+        $('#messages').html("");
+        for (msgData of data) {
+            var name = msgData.name;
+            var message = msgData.message;
+            var nameDiv = $('<div>', { 'class': 'name' }).text(name);
+            var messageDiv = $('<div>', { 'class': 'message' }).html(message);
+            var messageGroup = $('<div>', { 'class': 'message-group' }).append(nameDiv).append(messageDiv);
+            $('#messages').append(messageGroup);
+        }
+    });
+}
 
 $(document).ready(function() {
     
@@ -18,4 +32,9 @@ $(document).ready(function() {
             dataType: "json"
         });
     });
+    
+    fetchData();
+    setInterval(function() {
+        fetchData();
+    }, 5000);
 });
